487882e37993840bae6ecd59d6595873b0e2b9df
Wilmer Blanco <wilmervzla2000@gmail.com>
2013-12-01 18:48:22 -0430

    Update

--- registro.php
+++ registro.php
@@ -1,18 +1,10 @@
 <!doctype html>
-<html lang="es">
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Talemtum Joyas | Inicio</title>
-  <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/960_12_col.css">
-
-  <link rel="stylesheet" href="css/style.css" type="text/css" media="screen"/>
-
+	<meta charset="UTF-8">
+	<title>Talemtum Joyas | Registro</title>
 </head>
 <body>
-	<div class="fondoNegro colordegradado"></div>
 	<header>
     <div class="BarraSuperior">
     
@@ -63,9 +55,7 @@
       </div>
     </div>
   </header>
-  
-  <div class="container_12 contenedorprincipal">
-	<form class="FormRegistro" action="registrar.php" method="POST">
+	<form action="registrar.php" method="POST">
 	<?php
 		if (isset($_GET['error'])) {
 	?>
@@ -74,80 +64,24 @@
 	<?php
 		}
 	?>
-		
-		<div class="grid_4">
-		  <img src="img/business_user_add.png" alt="">
-		  
-		</div>
-		
-		<div class="grid_8">
-		  
-          <label for="nombre">Razon Social/Nombre*</label><br>
-          <input type="text" name="nombre" id="nombre"><br>
-          
-          <label for="rif">Rif/Cedula</label><br>
-          <input type="text" name="rif" id="rif"><br>
-                    
-          <label for="clave">Contrase&ntilde;a*</label><br>
-          <input type="password" name="clave" id="clave"><br>
-    
-                  <span>La clave debe tener mas de 8 caracteres</span><br>
-          
-          <label for="cclave">Repetir Contrase&ntilde;a*</label><br>
-          <input type="password" name="cclave" id="cclave"><br>
-          
-          <label for="correo">Correo*</label><br>
-          <input type="mail" name="correo" id="correo" placeholder="ejemplo@dominio.com"><br>
-          
-          <label for="direccion">Direcci&oacute;n</label><br>
-          <textarea cols="50" rows="5" name="direccion" id="direccion"></textarea><br>
-          
-          <label for="telefono">Telefono</label><br>
-          <input type="text" name="telefono" id="telefono"><br>
-          
-          <label for="celular">Celular</label><br>
-          <input type="text" name="celular" id="celular"><br>
-          <input type="submit" value="Registrar">
-		</div>
-
+		<label for="correo">Correo*</label>
+		<input type="mail" name="correo" id="correo" placeholder="ejemplo@dominio.com">
+		<label for="clave">Contrase&ntilde;a*</label>
+		<input type="password" name="clave" id="clave">
+		<span>La clave debe tener mas de 8 caracteres</span>
+		<label for="cclave">Repetir Contrase&ntilde;a*</label>
+		<input type="password" name="cclave" id="cclave">
+		<label for="rif">Rif/Cedula</label>
+		<input type="text" name="rif" id="rif">
+		<label for="nombre">Razon Social/Nombre*</label>
+		<input type="text" name="nombre" id="nombre">
+		<label for="direccion">Direcci&oacute;n</label>
+		<textarea name="direccion" id="direccion"></textarea>
+		<label for="telefono">Telefono</label>
+		<input type="text" name="telefono" id="telefono">
+		<label for="celular">Celular</label>
+		<input type="text" name="celular" id="celular">
+		<input type="submit" value="Registrar">
 	</form>
-  
-  </div>
-    <footer>
-    <div class="container_12 contfooter colordegradado2">
-      <img src="img/anillos.png" alt="" class="anillofooter">
-      <div class="grid_3 txtfooter">
-        <p>
-          Talentum Joyas
-        </p>
-        <ul>
-          <li>Dirección: Av. 20 con calle 28 y 29, Edificio Balmoral, Piso 1.</li>
-          <li>Teléfono: +58416-6572119</li>
-          <li>E-mail:</li>
-        </ul>
-      </div>
-      <div class="grid_2 txtfooter">
-        <p>
-          Sitemap
-        </p>
-        <ul>
-          <li><a href="#">Inicio</a></li>
-          <li><a href="#">Portafolio</a></li>
-          <li><a href="#">Contacto</a></li>
-          <li><a href="backend/menu.html.php">Admin</a></li>
-        </ul>
-      </div>
-      <div class="grid_3 txtfooter">
-        <p>
-          Redes Sociales
-        </p>
-            <ul class="listfootercontac listfootersocial">
-              <li><a href="#"><img src="img/fb.png" alt=""> Facebook</a></li>
-              <li><a href="#"><img src="img/tw.png" alt=""> Twitter</a></li>
-              <li><a href="#"><img src="img/yt.png" alt=""> Google+</a></li>
-            </ul>
-      </div>
-    </div>
-  </footer>
 </body>
 </html>
