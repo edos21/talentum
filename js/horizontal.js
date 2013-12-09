5142ca236a3e5d1ff0b4039c1411f444c61e4ad0
Eduardo Sanchez <edos21@gmail.com>
2013-12-09 13:38:28 -0430

    nuevo slider

---
+++ js/horizontal.js
@@ -0,0 +1,43 @@
+jQuery(function($){
+	'use strict';
+
+	// -------------------------------------------------------------
+	//   Basic Navigation
+	// -------------------------------------------------------------
+	(function () {
+		var $frame  = $('#basic');
+		var $slidee = $frame.children('ul').eq(0);
+		var $wrap   = $frame.parent();
+
+		// Call Sly on frame
+		$frame.sly({
+			horizontal: 1,
+			itemNav: 'basic',
+			smart: 1,
+			activateOn: 'click',
+			mouseDragging: 1,
+			touchDragging: 1,
+			releaseSwing: 1,
+			startAt: 3,
+			scrollBar: $wrap.find('.scrollbar'),
+			scrollBy: 1,
+			pagesBar: $wrap.find('.pages'),
+			activatePageOn: 'click',
+			speed: 300,
+			elasticBounds: 1,
+			easing: 'easeOutExpo',
+			dragHandle: 1,
+			dynamicHandle: 1,
+			clickBar: 1,
+
+			// Buttons
+			forward: $wrap.find('.forward'),
+			backward: $wrap.find('.backward'),
+			prev: $wrap.find('.prev'),
+			next: $wrap.find('.next'),
+			prevPage: $wrap.find('.prevPage'),
+			nextPage: $wrap.find('.nextPage')
+		});
+	}());
+});
+	
---
+++ test.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta charset="UTF-8">
+	<title>Talemtum Joyas | Inicio</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/960_12_col.css">
+	<!--<script type="text/javascript" src="js/jquery-2.0.3.min.js"></script>-->
+	<!--<script src="js/cufon-yui.js" type="text/javascript"></script>-->
+	<!--<script src="js/GreyscaleBasic.font.js" type="text/javascript"></script>-->
+	<!--  <link type="text/css" href="css/jquery.ui.theme.css" rel="stylesheet" />
+	<link type="text/css" href="css/jquery.ui.core.css" rel="stylesheet" />
+	<link type="text/css" href="css/jquery.ui.slider.css" rel="stylesheet" />-->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/osbp.css">
+	<link rel="stylesheet" href="css/horizontal.css">
+	<script language="JavaScript" type="text/javascript" src="local 5- 6x8_VR.37/files/KeyShotVR.js"></script>
+	<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>-->
+	<!--  <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>-->
+</head>
+<body>
+<div class="pagespan container">
+	<div class="wrap">
+
+		<div class="scrollbar">
+			<div class="handle" style="-webkit-transform: translateZ(0px) translateX(38px); width: 190px;">
+				<div class="mousearea"></div>
+			</div>
+		</div>
+
+		<div class="frame" id="basic" style="overflow: hidden;">
+			<ul class="clearfix" style="-webkit-transform: translateZ(0px) translateX(-228px); width: 6840px;">
+				<li>0</li><li>1</li><li>2</li><li class="active">3</li><li>4</li><li>5</li><li>6</li><li>7</li><li>8</li><li>9</li>
+				<li>10</li><li>11</li><li>12</li><li>13</li><li>14</li><li>15</li><li>16</li><li>17</li><li>18</li>
+				<li>19</li><li>20</li><li>21</li><li>22</li><li>23</li><li>24</li><li>25</li><li>26</li><li>27</li>
+				<li>28</li><li>29</li>
+			</ul>
+		</div>
+
+		<ul class="pages"><li class="active">1</li><li class="">2</li><li>3</li><li>4</li><li>5</li><li>6</li></ul>
+
+		
+	</div>
+</div>
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="js/vendor.js"></script>
+<script type="text/javascript" src="js/sly.min.js"></script>
+<script type="text/javascript" src="js/horizontal.js"></script>
+</body>
+</html>
