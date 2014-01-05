54c0e86d79ff265486d29dba1a030038635850a5
Eduardo <edos21@gmail.com>
2014-01-05 18:43:27 -0430

    Update eliminaCategoria.php

--- backend/eliminaCategoria.php
+++ backend/eliminaCategoria.php
@@ -1,8 +1,22 @@
 <?php
 
 	include '../lib/conexion.php';
-	include '../lib/seguridad.php';
+
+	try{
+		$sql = 'SELECT * FROM categoria WHERE id = :id';
+		$s = $pdo->prepare($sql);
+		$s->bindValue(':id', $_POST['id']);
+		$s->execute();
+	}
+	catch(PDOException $e){
+		echo "Ha ocurrido un error al eliminar";
+		exit();
+	}
 	
+	if ($row = $s->fetch()){
+		unlink('../img/'.$row['img']);
+	}
+
 	try{
 
 		$sql = 'DELETE FROM categoria WHERE id = :id';
