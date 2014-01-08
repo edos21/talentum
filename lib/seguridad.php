44891ccf97e14b3b9917032d3cfa364d349ad3c8
Eduardo <edos21@gmail.com>
2014-01-07 20:04:27 -0430

    Update seguridad.php

--- lib/seguridad.php
+++ lib/seguridad.php
@@ -1,10 +1,6 @@
 <?php
 
 
-if ($_SESSION['login'] != true){
-	header('location: ../ingreso.php?sesion');
-}
-
 if ($_SESSION['admin'] != true){
 	header ('location: ../backend/ingreso.php?sesion');
 }
