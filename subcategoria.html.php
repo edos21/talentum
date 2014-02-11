65625a20abf131dab6e10e451d0e23a98cd78664
Eduardo <edos21@gmail.com>
2014-02-11 08:55:41 -0430

    Update subcategoria.html.php

--- subcategoria.html.php
+++ subcategoria.html.php
@@ -183,7 +183,7 @@
           <li><a href="./">Inicio</a></li>
           <li><a href="categorias.html.php">Productos</a></li>
           <li><a href="contacto.php">Contacto</a></li>
-          <li><a href="backend/menu.html.php">Admin</a></li>
+          <li><a href="backend/ingreso.php">Admin</a></li>
         </ul>
       </div>
       <div class="grid_3 txtfooter">
