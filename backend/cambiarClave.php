0186cefafd9d2f76bfe3f37a692bf7c289066fd5
Eduardo <edos21@gmail.com>
2014-02-15 18:51:22 -0430

    Create cambiarClave.php

---
+++ backend/cambiarClave.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+    include '../lib/seguridad.php';
+    include '../includes/header.html';
+?>
+
+    <h2>Cambiar Clave</h2>
+    <div class="push_10">
+        <a class="BtnVolver" href="menu.html.php">Volver</a>
+    </div>
+	
+	<div class="grid_11">
+		<form method="post" action="claveCambiada.php">
+			<label for="clave">clave nueva</label>
+			<input type="password" name="clave" id="clave"><br>
+            <label for="claveDos">repetir clave</label>
+			<input type="password" name="claveDos" id="claveDos"><br>
+            <div class="Botonera" >
+                <input class="BtnGuardar" type="submit" value="Guardar">
+            </div>
+	</form>
+	
+	</div>
+	<div class="clearfix"></div>
+		
+
+<?php
+
+	include '../includes/footer.html';
+
+?>
