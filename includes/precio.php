bb3e2521bceb4fbeea54993718ac8a14d821db0e
Eduardo <edos21@gmail.com>
2014-02-16 15:10:59 -0430

    Create precio.php

---
+++ includes/precio.php
@@ -0,0 +1,22 @@
+<?php
+    try{
+        $sql = 'SELECT * FROM materiales';
+        
+        $s = $pdo->prepare($sql);
+        $s->execute();
+    } catch(PDOException $e){
+        echo 'error al seleccionar la tabla';
+        exit();
+    }
+    while($row = $s->fetch()){
+		$materiales[] = array(
+			'nombre'=>$row['nombre'],
+			'precio'=>$row['precio'],
+			'estado'=>$row['estado']);
+	}
+  foreach($materiales as $material): ?>
+    <p>Precio del <?php echo $material['nombre']; ?></p>
+    <p><?php echo $material['precio']; ?> $
+      <img src="img/Flecha<?php echo $material['estado']; ?>.png" alt="">
+    </p>
+  <?php endforeach; ?>
